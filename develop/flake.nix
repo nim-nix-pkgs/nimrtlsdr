ea68c290c2c323b6912c87c46359b7f382a07933
hugosenari <hugosenari@gmail.com>
2022-03-12 04:31:32 -0300

    chore: re index nimrtlsdr

---
+++ develop/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A Nim wrapper for librtlsdr'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimrtlsdr-develop.flake = false;
+  inputs.src-nimrtlsdr-develop.owner = "jpoirier";
+  inputs.src-nimrtlsdr-develop.ref   = "refs/heads/develop";
+  inputs.src-nimrtlsdr-develop.repo  = "nimrtlsdr";
+  inputs.src-nimrtlsdr-develop.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimrtlsdr-develop"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimrtlsdr-develop";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = ''A Nim wrapper for librtlsdr'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimrtlsdr-develop".url = "path:./develop";
+  inputs."nimrtlsdr-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-master".url = "path:./master";
+  inputs."nimrtlsdr-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-v0_1".url = "path:./v0_1";
+  inputs."nimrtlsdr-v0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-v0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-v0_1_1".url = "path:./v0_1_1";
+  inputs."nimrtlsdr-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-v0_1_2".url = "path:./v0_1_2";
+  inputs."nimrtlsdr-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-v0_1_3".url = "path:./v0_1_3";
+  inputs."nimrtlsdr-v0_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-v0_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nimrtlsdr-v0_1_4".url = "path:./v0_1_4";
+  inputs."nimrtlsdr-v0_1_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrtlsdr-v0_1_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
